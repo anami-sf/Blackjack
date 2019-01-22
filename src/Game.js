510ca8daa8b25a6559cf73cc697974d921672ab4
Ana <anami823@gmail.com>
2019-01-21 23:00:13 -0800

    Added Dealer Component which keeps track of the dealers hand and hand total(score)

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { CardArr } from './CardArr';
 import Deck from './Deck';
 import Player from './Player';
+import Dealer from './Dealer';
 
 class Game extends Component {
     constructor(){
@@ -9,7 +10,27 @@ class Game extends Component {
         this.state = {
             deck: CardArr,
             player: null,
-            dealer: [],
+            dealer: null,
+        }
+    }
+
+    deal = () => {
+        let deckCopy = this.state.deck.slice();
+        let topCard = deckCopy.pop(1);
+        console.log(topCard)
+        this.setState({deck: deckCopy});
+        console.log(deckCopy);
+
+        if (!this.state.dealer) {
+            let dealerHand = [];
+            dealerHand.push(topCard);
+            this.setState({dealer: dealerHand}); 
+            console.log('dealerHand' + dealerHand)
+        } else {
+            var hand = this.state.dealer
+            hand.push(topCard);
+            this.setState({dealer: hand}); 
+            console.log('hand' + hand)
         }
     }
 
@@ -40,7 +61,10 @@ class Game extends Component {
             <div>
                 {console.log(this.state.deck)}
                 {console.log(this.state.player)}
-                <input onClick={() => this.handleClick()} type="button" value="Deal"></input>
+                <input onClick={() => this.deal()} type="button" value="Deal"></input>
+                <input onClick={() => this.handleClick()} type="button" value="Hit"></input>
+                <p>Dealer Hand</p>
+                <Dealer CardArr = {this.state.dealer} />
                 <p>Player Hand</p>
                 <Player CardArr = {this.state.player} />
                 <p>Deck</p>
