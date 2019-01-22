69956090aa3093009cccbf87b695d64b96e49e10
Ana <anami823@gmail.com>
2019-01-21 19:14:32 -0800

    Commented out the Player componenet and all related functions/code and there are no errors. The deck looses a card every time the button Deal is clicked

--- src/Game.js
+++ src/Game.js
@@ -1,35 +1,18 @@
 import React, {Component} from 'react';
 import { CardArr } from './CardArr';
 import Deck from './Deck';
-import Player from './Player';
+//import Player from './Player';
 
 class Game extends Component {
     constructor(){
         super()
         this.state = {
             deck: CardArr,
-            player: [{
-                number: 21, 
-                suit: 'A', 
-                value: 21, 
-                key: `1A`,
-                img: '/CardImages/2H.jpg',
-            }],
+            player: [],
             dealer: [],
         }
     }
 
-/*
-    componentDidMount(){
-        let deckCopy = this.state.deck.slice();
-        let topCard = deckCopy.pop();
-        this.setState({deck: deckCopy});
-        let playerHand = this.state.deck.slice();
-        playerHand = playerHand.unshift(topCard);
-        this.setState({player: playerHand});
-        this.setState({playear: playerHand})
-    }
-*/
 
     handleClick = () => {
         
@@ -37,11 +20,12 @@ class Game extends Component {
         let topCard = deckCopy.pop();
         console.log(topCard)
         this.setState({deck: deckCopy});
+        console.log(deckCopy);
+        /* 
         let playerHand = this.state.player.slice();
         playerHand = playerHand.push(topCard);
         this.setState({player: playerHand});
-        console.log(deckCopy);
-        console.log(playerHand);
+        console.log(playerHand); */
     }
     
     
@@ -52,10 +36,10 @@ class Game extends Component {
                 {console.log(this.state.player)}
                 <input onClick={() => this.handleClick()} type="button" value="Deal"></input>
                 <p>Player Hand</p>
-                <Player CardArr = {this.state.player} />
                 <p>Deck</p>
                 <Deck CardArr = {this.state.deck} />
             </div>
+            //<Player CardArr = {this.state.player} />
         );
     }
 }
