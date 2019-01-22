9c0bcf65869f308293a5f88e4564f6bdc127eab0
Ana <anami823@gmail.com>
2019-01-21 22:25:58 -0800

    not a function error on player component

--- src/Player.js
+++ src/Player.js
@@ -2,26 +2,32 @@ import React from 'react';
 import Card from './Card';
 
 const Player = ({CardArr}) => {
-    if (CardArr) {
+    if (!CardArr){
+        return null;
+    } else {
+       /*  const Score = CardArr.reduce((acc, card) => {
+            return (acc + card.value);
+        }, 0); */
+        let Score = 0
         const Cards = CardArr.map(card => {
-            return(
+            return (    
                     <Card 
                         number = {card.number} 
                         suit = {card.suit} 
                         value = {card.value} 
                         image = {card.img}
-                        key ={`${card.number}{${card.suit}`}
+                        key = {`${card.number}{${card.suit}`}
                       />
             )
         })
 
         return (
                 <div>
+                    <p>Player Hand Total: {Score} </p>
                     {Cards}
                 </div>
         ) 
-    }
-    return null;           
+    }     
 }
 
 export default Player;
