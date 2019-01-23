8f066c8a52657a78eb0375282be45d66c3485527
Ana <anami823@gmail.com>
2019-01-23 15:36:07 -0800

    Score component now keeps score and declares a winner

--- src/CardArr.js
+++ src/CardArr.js
@@ -1,37 +1,3 @@
-
-
-/*
-export const CardArr =[
-    {number:1, suit:'H', value: 1, img: `/CardImages/2H.jpg`},
-    {number:2, suit:'D', value: 2, img: `/CardImages/2H.jpg`},
-    {number:3, suit:'S', value: 3, img: `/CardImages/2H.jpg`},
-];
-*/
-
- /*
-    for(const num of numArr){
-        for(const suit of suitArr){
-            const value= (num) => {
-                if (typeof num === 'number') {
-                    return num 
-                } else if (num === 'A') { 
-                    return 1
-                } else {
-                    return 10
-                }
-
-                const Cards = [];
-                const cardObj = {number:num, suit:suit, value: value(num), img: `/CardImages/${num}${suit}.jpg`}
-                Cards.push(cardObj);
-            }
-            
-            
-        }
-    }
-    
-
-*/
-
 const numArr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ];
 const suitArr = ['H', 'D', 'S', 'C' ];
 
--- src/Game.js
+++ src/Game.js
@@ -3,89 +3,99 @@ import { CardArr } from './CardArr';
 import Deck from './Deck';
 import Player from './Player';
 import { Dealer } from './Dealer';
+//import Winner from './Winner.js';
+import Score from './Score.js';
 
 class Game extends Component {
     constructor(){
         super()
         this.state = {
             deck: CardArr,
-            player: null,
-            playerHand: null,
-            PlayerHandTotal: 0,
-            dealerHandTotal: 0,
-            dealer: null,
-            winner: null,
+            playerHand: [],
+            dealerHand: null,
         }
     }
     
-    
-    Winner = () => {      
-        if (this.state.PlayerHandTotal >= 21){this.setState({winner: 'Player'})}
+    score = (cardArr) => {
+        if (cardArr) {          
+            const handTotal = cardArr.reduce((score, card) => {
+                return score + card.value
+            }, 0);
+            
+            return (handTotal);
+        } else {
+            return 0; 
+        }          
     }
+    
+    
+    
+    updatePlayerHand = (hand) => {
+        return this.setState({playerHand: hand});
+    }
+    
+    setDeck = (arr) => {
+        return this.setState({deck: arr});
+    }
+    
+    winner = () => {
+        if (this.state.winner >= 10){
+            this.setState({winner: 'Player'});
+        }
+    }
+    
+    hit = () => {
+        //take one card from deck 
+        let deckCopy = this.state.deck.slice();
+        let topCard = deckCopy.pop(1);
+        this.setDeck(deckCopy);
+        //Add card to player's hand
+        let hand = this.state.playerHand
+        hand.push(topCard);
+        this.setState({playerHand: hand}); 
+    }
+    
+    deal = () => {
         
-        deal = () => {
-
-            let deckCopy = this.state.deck.slice();
-            let topCard = deckCopy.pop(1);
-            console.log(topCard)
-            this.setState({deck: deckCopy});
-            console.log(deckCopy);
+        let deckCopy = this.state.deck.slice();
+        let topCard = deckCopy.pop();
+        //console.log(topCard)
+        this.setState({deck: deckCopy});
+        //console.log(deckCopy);
+        
+        if (!this.state.dealer) {
+            let dealerHand = [];
+            dealerHand.push(topCard);
+            this.setState({dealer: dealerHand}); 
+            //console.log('dealerHand' + dealerHand)
+        } else {
+            var hand = this.state.dealer
+            hand.push(topCard);
+            this.setState({dealer: hand}); 
+            //console.log('hand' + hand)
+        }
+    }
+    
+    //<Winner winner = {this.state.winner} PlayerHandTotal = {this.state.PlayerHandTotal}/>
+    render(){
+        return(
+            <div className = 'tc '>
+            <input className='ma4 ' onClick={() => this.deal()} type="button" value="Deal"></input>
+            <input onClick={() => this.hit()} type="button" value="Hit"></input>
             
-            if (!this.state.dealer) {
-                let dealerHand = [];
-                dealerHand.push(topCard);
-                this.setState({dealer: dealerHand}); 
-                console.log('dealerHand' + dealerHand)
-            } else {
-                var hand = this.state.dealer
-                hand.push(topCard);
-                this.setState({dealer: hand}); 
-                console.log('hand' + hand)
-            }
+            <Score playerHand = {this.state.playerHand} />
+            <h2>Dealer Hand</h2>
+            <Dealer CardArr = {this.state.dealer} />
+            <h2>Player Hand</h2>
+            <Player CardArr = {this.state.playerHand} />
+            <h2>Deck</h2>
+            <Deck CardArr = {this.state.deck} />
+            </div>
+            );
         }
         
-        hit = () => {
-
-            console.log('winner: ' + this.state.winner)
-            
-            let deckCopy = this.state.deck.slice();
-            let topCard = deckCopy.pop();
-            //console.log(topCard)
-            this.setState({deck: deckCopy});
-            //console.log(deckCopy);
-            
-            if (!this.state.player) {
-                let playerHand = [];
-                playerHand.push(topCard);
-                this.setState({player: playerHand}); 
-            } else {
-                var hand = this.state.player
-                hand.push(topCard);
-                this.setState({player: hand}); 
-            }
-            
-            this.Winner();          
-        }
-        
-        
-        render(){
-            return(
-                <div className = 'tc '>
-                <input className='ma4 ' onClick={() => this.deal()} type="button" value="Deal"></input>
-                <input onClick={() => this.hit()} type="button" value="Hit"></input>
-                <h2>Winner: {this.state.winner}</h2>
-                <h2>Dealer Hand</h2>
-                <Dealer CardArr = {this.state.dealer} />
-                <h2>Player Hand</h2>
-                <Player CardArr = {this.state.player} />
-                <h2>Deck</h2>
-                <Deck CardArr = {this.state.deck} />
-                </div>
-                );
-            }
-            
-        }
-        
-        export default Game;
-        
-        
+    }
+    
+    export default Game;
+    
+    
